3e239d3b3ad7d751ac3f485bc7ad5a1b20af3f28
monkollek <monkol.lek@yale.edu>
2020-05-07 02:27:08 +0000

    Change links to build38 on Gene Page

--- projects/gnomad/src/client/GenePage/GeneInfo.js
+++ projects/gnomad/src/client/GenePage/GeneInfo.js
@@ -16,10 +16,10 @@ const GeneInfo = ({ currentTranscript, gene }) => {
     stop,
   } = gene
 
-  const ensemblGeneUrl = `http://grch37.ensembl.org/Homo_sapiens/Gene/Summary?g=${geneId}`
-  const ensemblTranscriptUrl = `http://grch37.ensembl.org/Homo_sapiens/Transcript/Summary?t=${currentTranscript ||
+  const ensemblGeneUrl = `http://ensembl.org/Homo_sapiens/Gene/Summary?g=${geneId}`
+  const ensemblTranscriptUrl = `http://ensembl.org/Homo_sapiens/Transcript/Summary?t=${currentTranscript ||
     canonicalTranscript}`
-  const ucscUrl = `http://genome.ucsc.edu/cgi-bin/hgTracks?db=hg19&position=chr${chrom}%3A${start -
+  const ucscUrl = `http://genome.ucsc.edu/cgi-bin/hgTracks?db=hg38&position=chr${chrom}%3A${start -
     1}-${stop}`
   const geneCardsUrl = `http://www.genecards.org/cgi-bin/carddisp.pl?gene=${geneName}`
   const omimUrl = `http://omim.org/entry/${omimAccession}`
